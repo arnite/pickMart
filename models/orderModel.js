83daab93252cc8d883a3e53f9ab27c6ba3ad1a23
arnite <udembao429@gmail.com>
2025-01-28 13:22:18 +0100

    Implemented cart and order CRUD operations

---
+++ models/orderModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  products: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'completed'],
+    default: 'pending',
+  },
+  paymentInfo: {
+    type: String,
+    required: true,
+  },
+  shippingAddress: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Order', orderSchema);
